3a5fac410fbef31ee823302d9840c8e2d1a1d394
parakkalsreerag1234 <105154980+parakkalsreerag1234@users.noreply.github.com>
2025-10-05 19:09:08 +0400

    Update Asteroid Visualizer.py

--- Asteroid Visualizer.py
+++ Asteroid Visualizer.py
@@ -23,6 +23,7 @@ left, right = st.columns([1.2, 1])
 with left:
     st.subheader("🌍 Select Impact Location")
     st.write(" After calculating or viewing asteroid data please go down a to see the data")
+    st.warning("Dark mode recommended for less issues ")
     st.write("Click anywhere on the map — the last clicked area is selected as the impact location.")
     m = folium.Map(location=[20, 0], zoom_start=2, tiles=map_tile)
     st_data = st_folium(m, width=500, height=400)
